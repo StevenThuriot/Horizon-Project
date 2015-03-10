ef7fa356c82de4cbf557fe392b293932b73e0524
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2015-03-10 19:53:25 +0100

    Added solution

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿#region License
+
+//  
+// Copyright 2015 Steven Thuriot
+//  
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//    http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+
+# endregion
+	
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Horizon")]
+[assembly: AssemblyDescription(".NET Fast Very Late Binding invoker")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Steven Thuriot.be")]
+[assembly: AssemblyProduct("Horizon")]
+[assembly: AssemblyCopyright("Copyright © Thuriot.be")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("52e58ef9-092f-415d-bf1e-8a2e99af41cd")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
