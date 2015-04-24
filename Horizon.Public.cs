06aa389f14b2f40cfaacbe776d58028707b1bc3a
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2015-04-24 21:38:14 +0200

    Upgraded Horizon and implemented new publics

--- Horizon.Public.cs
+++ Horizon.Public.cs
@@ -2,6 +2,12 @@ namespace Horizon
 {
 	public partial class TypeInfo { }
 	public partial class TypeInfo<T> { }
+	public partial class SimpleParameterInfo { }
 	public partial interface ICaller { }
 	public partial interface IEventCaller { }
+    public partial interface IMethodCaller { }
+    public partial interface IMemberCaller { }
+    public partial interface IMemberCaller<in T> { }
+    public partial interface IPropertyCaller { }
+    public partial interface IPropertyCaller<in T> { }
 }
