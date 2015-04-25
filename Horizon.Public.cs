b187ef0b0eafa9df40d8d2283468d7608bd48e87
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2015-04-25 17:29:01 +0200

    Updated Horizon

--- Horizon.Public.cs
+++ Horizon.Public.cs
@@ -1,13 +1,24 @@
 namespace Horizon
 {
-	public partial class TypeInfo { }
-	public partial class TypeInfo<T> { }
-	public partial class SimpleParameterInfo { }
-	public partial interface ICaller { }
-	public partial interface IEventCaller { }
+    public partial class TypeInfo { }
+
+    public partial class TypeInfo<T> { }
+
+    public partial class SimpleParameterInfo { }
+
+    public partial interface ICaller { }
+
+    public partial interface IEventCaller { }
+
     public partial interface IMethodCaller { }
+
     public partial interface IMemberCaller { }
+
     public partial interface IMemberCaller<in T> { }
+
     public partial interface IPropertyCaller { }
+
     public partial interface IPropertyCaller<in T> { }
+
+    public partial interface IConstructorCaller { }
 }
